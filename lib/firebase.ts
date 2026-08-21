9c1db5965564b5fe8df840c2215ec2e4de25a4e4
Rasheedolaleye <babseminent12@gmail.com>
2026-08-21 05:25:33 +0100

    first commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,26 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_API,
-  authDomain: process.env.NEXT_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PROJECT_ID,
-  storageBucket: process.env.NEXT_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_APP_ID,
-  measurementId: process.env.NEXT_MEASUREMENT_ID,
-  databaseURL: "https://door-f4b64-default-rtdb.firebaseio.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const app = getApps().length
+  ? getApp()
+  : initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
+export const db = getDatabase(app);
 export const rtdb = getDatabase(app);
